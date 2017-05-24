ef5eed6b09ddb2a5e0949562f66c275d4db14219
kevinzhai <zhaike12321@gmail.com>
2017-05-24 17:18:11 -0500

    kz first view

--- public/js/addMarkers.js
+++ public/js/addMarkers.js
@@ -6,7 +6,6 @@ function initMap() {
 		zoom: 6
 	});
 
-
 	setMarkers(map);
 }
 
@@ -41,29 +40,86 @@ function setMarkers(map) {
 	b98 = parseTruckData(bleach98);
 	b02 = parseTruckData(bleach02);
 
-	function getCircle(magnitude) {
-        return {
-          path: google.maps.SymbolPath.CIRCLE,
-          fillColor: 'red',
-          fillOpacity: .2,
-          scale: Math.pow(2, magnitude) / 2,
-          strokeColor: 'white',
-          strokeWeight: .5
-        };
-      }
+	function getCircle(color, opacity, magnitude) {
+		return {
+			path: google.maps.SymbolPath.CIRCLE,
+			fillColor: color,
+			fillOpacity: opacity,
+			scale: Math.pow(2, magnitude) / 2,
+			strokeColor: 'white',
+			strokeWeight: .5
+		};
+	}
+
+	var icons = {
+		one: {
+			name: '<1% Bleached',
+			icon: getCircle('green', .5, 4),
+			pic: 'one.png'
+		},
+		two: {
+			name: '1-10% Bleached',
+			icon: getCircle('yellowgreen', .5, 4),
+			pic: 'two.png'
+		},
+		three: {
+			name: '10-30% Bleached',
+			icon: getCircle('yellow', .5, 4),
+			pic: 'three.png'
+		},
+		four: {
+			name: '30-60% Bleached',
+			icon: getCircle('Orange', .5, 4),
+			pic: 'four.png'
+		},
+		five: {
+			name: '>60% Bleached',
+			icon: getCircle('red', .5, 4),
+			pic: 'five.png'
+		}
+	};
+
+	
+	var legend = document.getElementById('legend');
+	for (var key in icons) {
+		var type = icons[key];
+		var name = type.name;
+		var icon = type.icon;
+		var div = document.createElement('div');
+		div.innerHTML = '<img src="pics/' + type.pic + '"> ' + '<font size="4">' + name + '</font>';
+		legend.appendChild(div);
+	}
+
+	map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend);
+
 
 	for (var i = 0; i < b98.length; i++) {
 		(function(index){
 			var reef = b98[index];
+
+			if (reef[3] == 1) {
+				var type_icon = icons.one.icon
+			} else if (reef[3] == 2) {
+				var type_icon = icons.two.icon
+			} else if (reef[3] == 3) {
+				var type_icon = icons.three.icon
+			}else if (reef[3] == 4) {
+				var type_icon = icons.four.icon
+			}else {
+				var type_icon = icons.five.icon
+			}
+
+
+
 			var marker = new google.maps.Marker({
 				position: {lat: reef[1], lng: reef[2]},
 				map: map,
-				icon: getCircle(5),
+				icon: type_icon,
 				title: reef[0]
 			});
 			var infowindow = new google.maps.InfoWindow({
-				content: '<div>'+
-				'<h1 color="black">' + reef[0] + '</h1>' +
+				content: '<div id="tooltip">'+
+				'<h1>' + reef[0] + '</h1>' +
 				'</div>'
 			});
 			marker.addListener('click', function() {
